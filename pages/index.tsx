b0dd5f6c0578278dc8ee17b196762b8173dd152e
Ailton Duarte <adidas.coyas@gmail.com>
2022-11-02 15:31:17 -0100

    data to teste footer

--- pages/index.tsx
+++ pages/index.tsx
@@ -4,5 +4,12 @@ import styles from "../styles/Home.module.css";
 import Layout from "../components/Layout";
 
 export default function Home() {
-	return <Layout>terra data</Layout>;
+	return (
+		<Layout>
+			terra data
+			<p>sdasdsdsa</p>
+			<p>sdasdsdasdsd</p>
+			<p>sadsdas</p>
+		</Layout>
+	);
 }
