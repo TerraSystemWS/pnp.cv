5ac1962778a4b837ba9d45a11988b4b2415e53d9
Ailton Duarte <adidas.coyas@gmail.com>
2022-11-02 16:33:52 -0100

    make it clean to deploy

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,14 +2,8 @@ import Head from "next/head";
 import Image from "next/image";
 import styles from "../styles/Home.module.css";
 import Layout from "../components/Layout";
+import logo from "public/logo1.png";
 
 export default function Home() {
-	return (
-		<Layout>
-			terra data
-			<p>sdasdsdsa</p>
-			<p>sdasdsdasdsd</p>
-			<p>sadsdas</p>
-		</Layout>
-	);
+	return <Layout>pnp</Layout>;
 }
