38c6d59b801f064f5bca61bfd948fb34ddfead34
Ailton Duarte <adidas.coyas@gmail.com>
2022-11-21 15:14:15 -0100

    new pacotes

--- next.config.js
+++ next.config.js
@@ -5,6 +5,14 @@ const nextConfig = {
 	images: {
 		domains: ["res.cloudinary.com"],
 	},
+	webpack(config) {
+		config.module.rules.push({
+			test: /\.svg$/,
+			use: ["@svgr/webpack"],
+		});
+
+		return config;
+	},
 };
 
 module.exports = nextConfig;
