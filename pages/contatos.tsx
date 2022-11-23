d8cf40f0edc3f4fb37dc6032461698b684ff3c0f
Ailton Duarte <adidas.coyas@gmail.com>
2022-11-23 14:10:01 -0100

    change contact page layout

--- pages/contatos.tsx
+++ pages/contatos.tsx
@@ -7,67 +7,98 @@ const api_link = process.env.NEXT_PUBLIC_STRAPI_URL;
 const CONTATOS = ({ social, contato, banners }: any) => {
 	return (
 		<Layout rsocial={social} contato={contato}>
-			<section className="bg-white dark:bg-gray-900">
-				<div className="py-8 lg:py-16 px-4 mx-auto max-w-screen-md">
-					<h2 className="mb-4 text-4xl tracking-tight font-extrabold text-center text-gray-900 dark:text-white">
-						Contate-Nos
-					</h2>
-					<p className="mb-8 lg:mb-16 font-light text-center text-gray-500 dark:text-gray-400 sm:text-xl">
-						Teve um problema com o site? Deseja comentar sobre o PNP ou o
-						Website? Precisa de mais detalhes sobre o PNP? Informe-Nos.
-					</p>
-					<form action="#" className="space-y-8">
-						<div>
+			<section className="text-gray-600 body-font relative">
+				<div className="container px-5 py-24 mx-auto flex sm:flex-nowrap flex-wrap">
+					<div className="lg:w-2/3 md:w-1/2 bg-gray-300 rounded-lg overflow-hidden sm:mr-10 p-10 flex items-end justify-start relative">
+						<iframe
+							width="100%"
+							height="100%"
+							className="absolute inset-0 "
+							frameBorder="0"
+							title="map"
+							marginHeight={0}
+							marginWidth={0}
+							scrolling="no"
+							src={contato.data.attributes.mapa}
+							// style={{filter: grayscale(1) contrast(1.2) opacity(0.4)}}
+						></iframe>
+						{/* {contato.data.attributes.mapa} */}
+						<div className="bg-white relative flex flex-wrap py-6 rounded shadow-md">
+							<div className="lg:w-1/2 px-6">
+								<h2 className="title-font font-semibold text-gray-900 tracking-widest text-xs">
+									ENDEREÇO
+								</h2>
+								<p className="mt-1">{contato.data.attributes.Local}</p>
+							</div>
+							<div className="lg:w-1/2 px-6 mt-4 lg:mt-0">
+								<h2 className="title-font font-semibold text-gray-900 tracking-widest text-xs">
+									EMAIL
+								</h2>
+								<a className="text-indigo-500 leading-relaxed">
+									{contato.data.attributes.email}
+								</a>
+								<h2 className="title-font font-semibold text-gray-900 tracking-widest text-xs mt-4">
+									Contato
+								</h2>
+								<p className="leading-relaxed">
+									{contato.data.attributes.phone}
+								</p>
+							</div>
+						</div>
+					</div>
+					<div className="lg:w-1/3 md:w-1/2 bg-white flex flex-col md:ml-auto w-full md:py-8 mt-8 md:mt-0">
+						<h2 className="text-gray-900 text-lg mb-1 font-medium title-font">
+							Contate-nos
+						</h2>
+						<p className="leading-relaxed mb-5 text-gray-600">
+							Post-ironic portland shabby chic echo park, banjo fashion axe
+						</p>
+						<div className="relative mb-4">
+							<label htmlFor="name" className="leading-7 text-sm text-gray-600">
+								Nome
+							</label>
+							<input
+								type="text"
+								id="name"
+								name="name"
+								className="w-full bg-white rounded border border-gray-300 focus:border-indigo-500 focus:ring-2 focus:ring-indigo-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out"
+							/>
+						</div>
+						<div className="relative mb-4">
 							<label
 								htmlFor="email"
-								className="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-300"
+								className="leading-7 text-sm text-gray-600"
 							>
-								Seu Email
+								Email
 							</label>
 							<input
 								type="email"
 								id="email"
-								className="shadow-sm bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-500 focus:border-primary-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500 dark:shadow-sm-light"
-								placeholder="seu_email@dominio.com"
-								required
+								name="email"
+								className="w-full bg-white rounded border border-gray-300 focus:border-indigo-500 focus:ring-2 focus:ring-indigo-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out"
 							/>
 						</div>
-						<div>
-							<label
-								htmlFor="subject"
-								className="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-300"
-							>
-								Assunto
-							</label>
-							<input
-								type="text"
-								id="subject"
-								className="block p-3 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 shadow-sm focus:ring-primary-500 focus:border-primary-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500 dark:shadow-sm-light"
-								placeholder="Deixe-nos saber como podemos ajudá-lo"
-								required
-							/>
-						</div>
-						<div className="sm:col-span-2">
+						<div className="relative mb-4">
 							<label
 								htmlFor="message"
-								className="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-400"
+								className="leading-7 text-sm text-gray-600"
 							>
-								Sua mensagem
+								Mensagem
 							</label>
 							<textarea
 								id="message"
-								rows={6}
-								className="block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg shadow-sm border border-gray-300 focus:ring-primary-500 focus:border-primary-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500"
-								placeholder="Deixe um comentario..."
+								name="message"
+								className="w-full bg-white rounded border border-gray-300 focus:border-indigo-500 focus:ring-2 focus:ring-indigo-200 h-32 text-base outline-none text-gray-700 py-1 px-3 resize-none leading-6 transition-colors duration-200 ease-in-out"
 							></textarea>
 						</div>
-						<button
-							type="submit"
-							className="py-3 px-5 text-sm font-medium text-center text-white rounded-lg bg-amarelo-ouro sm:w-fit hover:bg-castanho-claro focus:ring-4 focus:outline-none focus:ring-amarelo-ouro"
-						>
-							enviar a mensagem
+						<button className="text-white bg-indigo-500 border-0 py-2 px-6 focus:outline-none hover:bg-indigo-600 rounded text-lg">
+							Enviar
 						</button>
-					</form>
+						<p className="text-xs text-gray-500 mt-3">
+							Chicharrones blog helvetica normcore iceland tousled brook viral
+							artisan.
+						</p>
+					</div>
 				</div>
 			</section>
 		</Layout>
