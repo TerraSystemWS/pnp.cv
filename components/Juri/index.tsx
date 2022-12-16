ac6878410609147d7faec8d02c1e49bce367a30c
Ailton Duarte <adidas.coyas@gmail.com>
2022-12-16 14:29:29 -0100

    add optional values

--- components/Juri/index.tsx
+++ components/Juri/index.tsx
@@ -9,7 +9,7 @@ const Juri = (props: any) => {
 			<div className="py-8 px-4 mx-auto max-w-screen-xl lg:py-16 lg:px-6 ">
 				<div className="mx-auto max-w-screen-sm text-center mb-8 lg:mb-16">
 					<h2 className="mb-4 text-4xl tracking-tight font-extrabold text-amarelo-ouro dark:text-white">
-						JÚRI DA {props.dados[0].edicao}ª EDIÇÃO
+						JÚRI DA {props.dados[0]?.edicao}ª EDIÇÃO
 					</h2>
 					<p className="font-light text-gray-500 lg:mb-16 sm:text-xl dark:text-gray-400">
 						Conheça mais sobre a comissão dos jurados
