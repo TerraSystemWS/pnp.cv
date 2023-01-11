323ddea29545718b578cb4829f4e1d6ba862042d
Ailton Duarte <adidas.coyas@gmail.com>
2023-01-11 17:34:19 -0100

    create upload component

--- pages/inscricao/[id].tsx
+++ pages/inscricao/[id].tsx
@@ -7,6 +7,7 @@ const qs = require("qs");
 import toast, { Toaster } from "react-hot-toast";
 import { Table } from "flowbite-react";
 import { IoTrashOutline } from "react-icons/io5";
+import Fileupload from "../../components/Fileupload";
 // import { normalizeRouteRegex } from "next/dist/lib/load-custom-routes";
 
 // link para a url do api
@@ -266,11 +267,6 @@ const Inscrever = ({ social, contato, edicao, navbar, inscricao }: any) => {
     } catch (err) {
       // console.log("erro:" + err);
     }
-  };
-
-  // submeter os ficheiros
-  const onSubmitFicheiros: SubmitHandler<Inputs> = async (data: any) => {
-    alert("submeter ficheiro");
   };
 
   const deleteFile = async () => {
@@ -920,44 +916,7 @@ const Inscrever = ({ social, contato, edicao, navbar, inscricao }: any) => {
                 <label className="block text-sm font-medium text-gray-700">
                   Documentos
                 </label>
-                <form onSubmit={handleSubmit(onSubmitFicheiros)}>
-                  <div className="mt-1 flex justify-center rounded-md border-2 border-dashed border-gray-300 px-6 pt-5 pb-6">
-                    <div className="space-y-1 text-center">
-                      <svg
-                        className="mx-auto h-12 w-12 text-gray-400"
-                        stroke="currentColor"
-                        fill="none"
-                        viewBox="0 0 48 48"
-                        aria-hidden="true"
-                      >
-                        <path
-                          d="M28 8H12a4 4 0 00-4 4v20m32-12v8m0 0v8a4 4 0 01-4 4H12a4 4 0 01-4-4v-4m32-4l-3.172-3.172a4 4 0 00-5.656 0L28 28M8 32l9.172-9.172a4 4 0 015.656 0L28 28m0 0l4 4m4-24h8m-4-4v8m-12 4h.02"
-                          strokeWidth={2}
-                          strokeLinecap="round"
-                          strokeLinejoin="round"
-                        />
-                      </svg>
-                      <div className="flex text-sm text-gray-600">
-                        <label
-                          htmlFor="file-upload"
-                          className="relative cursor-pointer rounded-md bg-white font-medium text-amarelo-ouro focus-within:outline-none focus-within:ring-2 focus-within:ring-indigo-500 focus-within:ring-offset-2 hover:text-amarelo-escuro"
-                        >
-                          <span>Upload dos Ficheiros</span>
-                          <input
-                            id="file-upload"
-                            name="file-upload"
-                            type="file"
-                            className="sr-only"
-                          />
-                        </label>
-                        <p className="pl-1">(link ainda não disponivel)</p>
-                      </div>
-                      <p className="text-xs text-gray-500">
-                        PNG, JPG, GIF up to 20MB
-                      </p>
-                    </div>
-                  </div>
-                </form>
+                <Fileupload />
               </div>
             </div>
           </div>
