af80ae882161600187faecedb7adb6abf9c1462a
Ailton Duarte <adidas.coyas@gmail.com>
2023-01-11 11:00:02 -0100

    type?

--- components/Layout/index.tsx
+++ components/Layout/index.tsx
@@ -6,28 +6,28 @@ import Footer from "../Footer";
 import React, { ReactNode } from "react";
 import Meta from "../Meta";
 interface Props {
-	children?: ReactNode;
-	rsocial: ReactNode;
-	contato: ReactNode;
-	navbar: ReactNode;
+  children?: ReactNode;
+  rsocial: ReactNode;
+  contato: ReactNode;
+  navbar: ReactNode;
 }
 
 const Layout = ({ children, rsocial, contato, navbar }: Props) => (
-	// <UserProvider value={{ user, loading }}>
-	<div className="">
-		<Head>
-			<Meta />
-		</Head>
+  // <UserProvider value={{ user, loading }}>
+  <div className="">
+    <Head>
+      <Meta />
+    </Head>
 
-		<header>
-			<NavBar navbar={navbar} />{" "}
-		</header>
+    <header>
+      <NavBar navbar={navbar} />{" "}
+    </header>
 
-		<div className="mt-28">{children}</div>
+    <div className="mt-28">{children}</div>
 
-		<Footer rsocial={rsocial} contato={contato} navbar={navbar} />
-	</div>
-	// </UserProvider>
+    <Footer rsocial={rsocial} contato={contato} navbar={navbar} />
+  </div>
+  // </UserProvider>
 );
 
 export default Layout;
