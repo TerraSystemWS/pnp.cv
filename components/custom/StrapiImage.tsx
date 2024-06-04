c1892f4ef34157beede87f6b8e546d82f4008f54
Ailton Duarte <adidas.coyas@gmail.com>
2024-06-04 03:27:41 -0100

    fix: optimized images

--- components/custom/StrapiImage.tsx
+++ components/custom/StrapiImage.tsx
@@ -2,39 +2,41 @@ import Image from "next/image";
 import { getStrapiMedia } from "../../lib/utils";
 
 interface StrapiImageProps {
-  src: string;
-  alt: string;
-  height: number;
-  width: number;
-  className?: string;
+	src: string;
+	alt: string;
+	height: number;
+	width: number;
+	className?: string;
 }
 
 const imageLoader = ({ src }: any) => {
-  return src
-}
+	return src;
+};
 
 export function StrapiImage({
-  src,
-  alt,
-  width,
-  height=width,
-  className,
+	src,
+	alt,
+	width,
+	height = width,
+	className,
 }: Readonly<StrapiImageProps>) {
-  if (!src) return null;
-  const imageUrl = getStrapiMedia(src);
-  const imageFallback = `https://placehold.co/${width}x${height}`;
+	if (!src) return null;
+	const imageUrl = getStrapiMedia(src);
+	const imageFallback = `https://placehold.co/${width}x${height}`;
 
-  // console.log("imageUrl dento strapiImage");
-  // console.log(imageUrl);
-  // console.log(imageFallback);
-  return (
-    <Image
-      // loader={imageLoader}// funciona mas retira a otimizaçao de imagens
-      src={imageUrl ?? imageFallback}
-      alt={alt}
-      height={height}
-      width={width}
-      className={className}
-    />
-  );
+	// console.log("imageUrl dento strapiImage");
+	// console.log(imageUrl);
+	// console.log(imageFallback);
+	return (
+		<img
+			// loader={imageLoader}// funciona mas retira a otimizaçao de imagens
+			src={imageUrl ?? imageFallback}
+			alt={alt}
+			// height={height}
+			// width={width}
+			height={`${height}px`}
+			width={`${width}px`}
+			className={className}
+		/>
+	);
 }
