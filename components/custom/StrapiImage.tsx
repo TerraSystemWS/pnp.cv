816e3ffe9f0abd24cbe84b5ef8240dd3f7bebaa6
Ailton Duarte <adidas.coyas@gmail.com>
2024-11-24 16:47:00 -0100

    fix: clean code

--- components/custom/StrapiImage.tsx
+++ components/custom/StrapiImage.tsx
@@ -1,53 +1,53 @@
-import Image from "next/image";
-import { getStrapiMedia } from "../../lib/utils";
+import Image from "next/image"
+import { getStrapiMedia } from "../../lib/utils"
 
 interface StrapiImageProps {
-	src: string;
-	alt: string;
-	height: number;
-	width: number;
-	className?: string;
+  src: string
+  alt: string
+  height: number
+  width: number
+  className?: string
 }
 interface StrapiDataProps {
-	src: string;
-	className?: string;
+  src: string
+  className?: string
 }
 const imageLoader = ({ src }: any) => {
-	return src;
-};
+  return src
+}
 
 export function StrapiImage({
-	src,
-	alt,
-	width,
-	height = width,
-	className,
+  src,
+  alt,
+  width,
+  height = width,
+  className,
 }: Readonly<StrapiImageProps>) {
-	if (!src) return null;
-	const imageUrl = getStrapiMedia(src);
-	const imageFallback = `https://placehold.co/${width}x${height}`;
+  if (!src) return null
+  const imageUrl = getStrapiMedia(src)
+  const imageFallback = `https://placehold.co/${width}x${height}`
 
-	// console.log("imageUrl dento strapiImage");
-	// console.log(imageUrl);
-	// console.log(imageFallback);
-	return (
-		<img
-			// loader={imageLoader}// funciona mas retira a otimizaçao de imagens
-			src={imageUrl ?? imageFallback}
-			alt={alt}
-			// height={height}
-			// width={width}
-			height={`${height}px`}
-			width={`${width}px`}
-			className={className}
-		/>
-	);
+  // console.log("imageUrl dento strapiImage");
+  // console.log(imageUrl);
+  // console.log(imageFallback);
+  return (
+    <img
+      // loader={imageLoader}// funciona mas retira a otimizaçao de imagens
+      src={imageUrl ?? imageFallback}
+      alt={alt}
+      // height={height}
+      // width={width}
+      height={`${height}px`}
+      width={`${width}px`}
+      className={className}
+    />
+  )
 }
 
 export function StrapiData({ src }: Readonly<StrapiDataProps>) {
-	if (!src) return " ";
-	const imageUrl = getStrapiMedia(src);
-	let srcFinal = { imageUrl };
+  if (!src) return " "
+  const imageUrl = getStrapiMedia(src)
+  let srcFinal = { imageUrl }
 
-	return srcFinal;
+  return srcFinal
 }
