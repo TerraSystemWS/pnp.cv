256c50d45cdb7919bed959bfe886e2e7a26dba6e
Ailton Duarte <adidas.coyas@gmail.com>
2024-11-24 23:12:04 -0100

    feat: new regulamentos layout

--- pages/regulamentos.tsx
+++ pages/regulamentos.tsx
@@ -1,259 +1,180 @@
-import Layout from "../components/Layout";
-import { fetcher } from "../lib/api";
-import showdown from "showdown";
-// import Link from "next/link";
-import Head from "next/head";
-const qs = require("qs");
-import { useFetchUser } from "../lib/authContext";
+import Layout from "../components/Layout"
+import { fetcher } from "../lib/api"
+import showdown from "showdown"
+import Head from "next/head"
+import { useFetchUser } from "../lib/authContext"
+import qs from "qs"
+import { useEffect } from "react"
 
-// link para a url do api
-const api_link = process.env.NEXT_PUBLIC_STRAPI_URL;
+// link to API URL
+const api_link = process.env.NEXT_PUBLIC_STRAPI_URL
 
 const Regulamentos = ({ social, contato, edicao, navbar }: any) => {
-	const { user, loading } = useFetchUser();
-	// const createMarkup = (values: any) => {
-	//   // const values =
-	//   const converter = new showdown.Converter();
-	//   const html = converter.makeHtml(values);
-	//   return { __html: html };
-	// };
+  const { user, loading } = useFetchUser()
 
-	const createMarkup = (values: any) => {
-		// console.log(values);
-		if (typeof values !== "string") {
-			// Handle the case when values is not a string, such as logging an error or returning a default value.
-			console.error("Error: 'values' is not a string.");
-			return { __html: "" }; // or any default value you want to return
-		}
+  // Markdown converter function
+  const createMarkup = (content: string) => {
+    if (typeof content !== "string") {
+      console.error("Error: Content is not a string.")
+      return { __html: "" }
+    }
+    const converter = new showdown.Converter()
+    return { __html: converter.makeHtml(content) }
+  }
 
-		const converter = new showdown.Converter();
-		const html = converter.makeHtml(values);
-		return { __html: html };
-	};
+  // Map Regulations
+  const Regulamentos = edicao.attributes.regulamentos.map(
+    (regulamento: any, index: number) => ({
+      id: index,
+      titulo: regulamento.titulo,
+      descricao: createMarkup(regulamento.descricao),
+    })
+  )
 
-	let Categoria: any = [];
-	let Regulamentos: any = [];
-	// create Juris objetct
-	/**
-	 * fiz um harded code com o numero de edicoes que tem de ser mudade urgente
-	 * antis tinha essa linha edicao.data.map((value: any, index: any) => {
-	 */
+  // Map Categories
+  const Categoria = edicao.attributes.categoria.map(
+    (categoria: any, index: number) => ({
+      id: index,
+      titulo: categoria.titulo,
+      slug: categoria.titulo.replace(/ /g, "_"),
+      descricao: createMarkup(categoria.descricao),
+    })
+  )
 
-	console.log("edicao");
-	console.log(edicao);
-	edicao.attributes.regulamentos.map((value2: any, index2: any) => {
-		// console.log("value2 :: regulamentos");
-		// console.log(value2.titulo);
-		// console.log(value2.descricao);
-		//  console.log(value2.foto.data?.attributes.url);
-		Regulamentos[index2] = {
-			id: index2,
-			titulo: value2.titulo,
-			descricao: createMarkup(value2.descricao),
-		};
-	});
+  // Scroll behavior adjustment for anchor links
+  useEffect(() => {
+    // Adiciona um evento de rolagem para os links com âncoras
+    const links = document.querySelectorAll('a[href^="#"]')
 
-	edicao.attributes.categoria.map((categs: any, index3: any) => {
-		Categoria[index3] = {
-			id: index3,
-			titulo: categs.titulo,
-			slug: categs.titulo.replace(/ /g, "_"),
-			descricao: createMarkup(categs.descricao),
-		};
-	});
+    links.forEach((link) => {
+      link.addEventListener("click", (e) => {
+        e.preventDefault()
+        const targetId = link.getAttribute("href")?.substring(1)
+        const targetElement = document.getElementById(targetId)
 
-	// edicao.data.map((value: any, index: any) => {
-	// 	value.attributes.regulamentos.map((value2: any, index2: any) => {
-	// 		// console.log(value2.foto.data?.attributes.url);
-	// 		Regulamentos[index2] = {
-	// 			id: index2,
-	// 			titulo: value2.titulo,
-	// 			descricao: createMarkup(value2.descricao),
-	// 		};
-	// 	});
-	// 	// get dados da categoria
-	// 	value.attributes.categoria.map((categs: any, index3: any) => {
-	// 		Categoria[index3] = {
-	// 			id: index3,
-	// 			titulo: categs.titulo,
-	// 			slug: categs.titulo.replace(/ /g, "_"),
-	// 			descricao: createMarkup(categs.descricao),
-	// 		};
-	// 	});
-	// });
+        if (targetElement) {
+          window.scrollTo({
+            top: targetElement.offsetTop - 230, // Ajuste o valor conforme a altura do menu
+            behavior: "smooth",
+          })
+        }
+      })
+    })
+  }, [])
 
-	// const a = createMarkup(Regulamentos[0].descricao);
+  return (
+    <Layout rsocial={social} contato={contato} navbar={navbar} user={user}>
+      <Head>
+        <title>{`Regulamento - Prémio Nacional De Publicidade`}</title>
+        <meta name="description" content={Regulamentos[0]?.titulo || " "} />
+      </Head>
 
-	return (
-		<Layout rsocial={social} contato={contato} navbar={navbar} user={user}>
-			<Head>
-				<title>Regulamento - Prémio Nacional De Publicidade</title>
-				<meta
-					name="description"
-					content={Regulamentos[0]?.titulo || " "}
-				/>
-			</Head>
-			{/* <pre>{JSON.stringify(edicao, null, 2)}</pre> */}
-			<div className="">
-				<div className="bg-gray-50">
-					<div className="mx-auto max-w-7xl py-12 px-4 sm:px-6">
-						<h2 className="text-2xl text-center mb-8 font-bold tracking-tight text-gray-900 sm:text-4xl">
-							<span className="block text-amarelo-ouro">
-								{Regulamentos[0]?.titulo}
-							</span>
-						</h2>
-					</div>
-				</div>
-			</div>
-			<section className="text-gray-600 body-font overflow-hidden">
-				<div className="container px-5 py-24 mx-auto">
-					<div className="-my-8 divide-y-2 divide-gray-100">
-						{/* {Categoria.map((values: any) => ( */}
-						<div className="py-8 flex flex-wrap md:flex-nowrap">
-							{/* <div className="md:w-64 md:mb-0 mb-6 flex-shrink-0 flex flex-col">
-									<span className="font-semibold title-font text-gray-700">
-										CATEGORY #{values.id}
-									</span>
-									<span className="mt-1 text-gray-500 text-sm">
-										12 Jun 2019
-									</span>
-								</div> */}
-							<div className="md:flex-grow">
-								<div className="leading-relaxed">
-									<span
-										dangerouslySetInnerHTML={
-											Regulamentos[0]?.descricao
-										}
-									/>
-								</div>
-							</div>
-						</div>
-						{/* ))} */}
-					</div>
-				</div>
-			</section>
-			{/* intro to categorias */}
-			<h1 className="text-4xl font-medium text-gray-700 text-center mt-6 bg-gray-100 pt-10">
-				{" "}
-				Categorias de Prémios
-			</h1>{" "}
-			<section className="text-gray-600 body-font overflow-hidden bg-gray-100">
-				<div className="container px-5 py-24 mx-auto">
-					<div className="-my-8 divide-y-2 divide-gray-300">
-						{Categoria.map((values: any) => (
-							<div
-								key={values.id}
-								className="py-8 flex flex-wrap md:flex-nowrap"
-							>
-								{/* <div className="md:w-64 md:mb-0 mb-6 flex-shrink-0 flex flex-col">
-									<span className="font-semibold title-font text-gray-700">
-										CATEGORY #{values.id+1}
-									</span>
-									<span className="mt-1 text-gray-500 text-sm">
-										12 Jun 2019
-									</span>
-								</div> */}
-								<div className="md:flex-grow">
-									<h2 className="text-2xl font-medium text-gray-900 title-font mb-2">
-										<a id={values.slug}>{values.titulo}</a>
-									</h2>
-									<div className="leading-relaxed">
-										<span
-											dangerouslySetInnerHTML={
-												values.descricao
-											}
-										/>
-									</div>
-								</div>
-							</div>
-						))}
-					</div>
-				</div>
-			</section>
-		</Layout>
-	);
-};
+      <section className="bg-gradient-to-r from-[#e6e2d8] via-[#dbdbdb] to-[#e6e2d8] py-12 px-6 sm:px-12 ">
+        <h2 className="text-3xl sm:text-4xl font-serif font-extrabold text-transparent mb-8 text-center leading-tight bg-gradient-to-r from-yellow-500 to-orange-500 bg-clip-text">
+          <span>{Regulamentos[0]?.titulo}</span>
+        </h2>
 
-export default Regulamentos;
+        <div className="max-w-7xl mx-auto px-4 sm:px-6">
+          <div className="py-8">
+            <div className="leading-relaxed text-xl text-gray-800">
+              <span dangerouslySetInnerHTML={Regulamentos[0]?.descricao} />
+            </div>
+          </div>
+        </div>
+      </section>
 
-export async function getServerSideProps() {
-	const query = qs.stringify(
-		{ sort: ["N_Edicao:DESC"] },
-		{ encodeValuesOnly: true }
-	);
+      {/* Categories Section */}
+      <section className="bg-gradient-to-r from-gray-100 via-gray-200 to-gray-100 text-gray-600 body-font">
+        <h1 className="mb-10 text-5xl font-extrabold text-gray-800 text-center mt-6 bg-gradient-to-r from-yellow-500 to-orange-500 text-transparent bg-clip-text pt-10">
+          Categorias de Prémios
+        </h1>
 
-	const [rsocials, contato, edicaoResponse, navbarResponse] =
-		await Promise.all([
-			fetcher(`${api_link}/api/redes-social?populate=*`),
-			fetcher(`${api_link}/api/contato`),
-			fetcher(`${api_link}/api/edicoes?_limit=1&populate=deep&${query}`),
-			fetcher(`${api_link}/api/menus?populate=deep`),
-		]);
+        <div className="bg-white shadow max-w-7xl mx-auto px-6 sm:px-12 py-24 flex">
+          {/* Box fixo à esquerda com links para as categorias */}
+          <div className="w-64 fixed top-32 left-1 z-5 bg-white rounded-xl shadow-lg p-6 transition-all duration-300 ease-in-out ">
+            <h2 className="text-xl font-bold text-transparent bg-gradient-to-r from-yellow-500 to-orange-500 bg-clip-text mb-4 uppercase">
+              Categorias
+            </h2>
 
-	const edicao = edicaoResponse?.data?.[0] || null;
+            <ul className="space-y-3">
+              {Categoria.map((category) => (
+                <li key={category.id} className="group relative">
+                  <a
+                    href={`#${category.slug}`}
+                    className="text-lg text-gray-800 hover:text-yellow-500 transition-colors duration-300 group-hover:pl-2"
+                  >
+                    {category.titulo}
+                  </a>
+                  <div className="absolute inset-y-0 left-0 w-1 bg-yellow-500 scale-x-0 group-hover:scale-x-100 transition-all duration-300"></div>
+                </li>
+              ))}
+            </ul>
+          </div>
 
-	const navbar =
-		navbarResponse?.data?.flatMap((menu: any) =>
-			menu.attributes.items.data.map((item: any) => ({
-				name: item.attributes.title,
-				link: item.attributes.url,
-			}))
-		) || [];
+          {/* Seção principal com conteúdo das categorias */}
+          <div className="w-full">
+            <div className="divide-y-4 divide-gray-300">
+              {Categoria.map((category) => (
+                <div
+                  key={category.id}
+                  className="py-16 flex flex-wrap md:flex-nowrap gap-12"
+                >
+                  <div className="md:flex-grow bg-white p-12 rounded-xl shadow-xl hover:shadow-2xl transition-shadow duration-300 ease-in-out">
+                    <h2 className="text-4xl font-bold text-transparent title-font mb-4 text-center tracking-wide bg-gradient-to-r from-yellow-500 to-orange-500 bg-clip-text">
+                      <a
+                        id={category.slug}
+                        className="hover:text-amarelo-ouro transition-colors duration-300"
+                      >
+                        {category.titulo}
+                      </a>
+                    </h2>
 
-	return {
-		props: {
-			social: rsocials,
-			contato,
-			edicao,
-			navbar,
-		},
-	};
+                    <div className="leading-relaxed text-lg text-gray-800">
+                      <span dangerouslySetInnerHTML={category.descricao} />
+                    </div>
+                  </div>
+                </div>
+              ))}
+            </div>
+          </div>
+        </div>
+      </section>
+    </Layout>
+  )
 }
 
-// This gets called on every request
-// export async function getServerSideProps2() {
-// 	// Fetch data from external API
-// 	const query = qs.stringify(
-// 		{
-// 			sort: ["N_Edicao:asc"],
-// 		},
-// 		{
-// 			encodeValuesOnly: true, // prettify URL
-// 		}
-// 	);
+export default Regulamentos
 
-// 	// GET: links para as redes sociais
-// 	const rsocials = await fetcher(`${api_link}/api/redes-social?populate=*`);
-// 	// GET: dados para contatos
-// 	const contato = await fetcher(`${api_link}/api/contato`);
+export async function getServerSideProps() {
+  const query = qs.stringify(
+    { sort: ["N_Edicao:DESC"] },
+    { encodeValuesOnly: true }
+  )
 
-// 	// GET: dados dos juris, categorias
-// 	const edicao = await fetcher(
-// 		`${api_link}/api/edicoes?_sort=id:DESC&_limit=1&populate=deep&${query}`
-// 	);
-// 	// GET: dados do navbar
-// 	const navbar = await fetcher(`${api_link}/api/menus?populate=deep`);
-// 	//get links for menu
-// 	let dlink: any = [];
-// 	navbar.data.map((value: any) => {
-// 		value.attributes.items.data.map((value: any, index: any) => {
-// 			// value.attributes.title;
-// 			// value.attributes.url;
-// 			// console.log(value);
-// 			dlink[index] = {
-// 				name: value.attributes.title,
-// 				link: value.attributes.url,
-// 			};
-// 		});
-// 	});
+  const [rsocials, contato, edicaoResponse, navbarResponse] = await Promise.all(
+    [
+      fetcher(`${api_link}/api/redes-social?populate=*`),
+      fetcher(`${api_link}/api/contato`),
+      fetcher(`${api_link}/api/edicoes?_limit=1&populate=deep&${query}`),
+      fetcher(`${api_link}/api/menus?populate=deep`),
+    ]
+  )
 
-// 	// Pass data to the page via props
-// 	return {
-// 		props: {
-// 			social: rsocials,
-// 			contato,
-// 			edicao: edicao && edicao.data && edicao.data[0],
-// 			navbar: dlink,
-// 		},
-// 	};
-// }
+  const edicao = edicaoResponse?.data?.[0] || null
+  const navbar =
+    navbarResponse?.data?.flatMap((menu: any) =>
+      menu.attributes.items.data.map((item: any) => ({
+        name: item.attributes.title,
+        link: item.attributes.url,
+      }))
+    ) || []
+
+  return {
+    props: {
+      social: rsocials,
+      contato,
+      edicao,
+      navbar,
+    },
+  }
+}
