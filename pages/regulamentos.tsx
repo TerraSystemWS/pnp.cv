668317d9054e92a0ba1b9b43222fd4822077c042
Ailton Duarte <adidas.coyas@gmail.com>
2024-11-25 01:29:55 -0100

    fix: ts types

--- pages/regulamentos.tsx
+++ pages/regulamentos.tsx
@@ -49,8 +49,8 @@ const Regulamentos = ({ social, contato, edicao, navbar }: any) => {
     links.forEach((link) => {
       link.addEventListener("click", (e) => {
         e.preventDefault()
-        const targetId = link.getAttribute("href")?.substring(1)
-        const targetElement = document.getElementById(targetId)
+        const targetId: any = link.getAttribute("href")?.substring(1)
+        const targetElement: any = document.getElementById(targetId)
 
         if (targetElement) {
           window.scrollTo({
