6e801ee1203e4593a8286df313eb4af90c2647a9
Ailton Duarte <adidas.coyas@gmail.com>
2024-11-29 16:21:28 -0100

    feat; new LoadSpinner

---
+++ components/LoadingSpinner/index.tsx
@@ -0,0 +1,12 @@
+// LoadingSpinner.js
+import React from "react"
+
+const LoadingSpinner = () => {
+  return (
+    <div className="flex justify-center items-center h-screen">
+      <div className="w-16 h-16 border-4 border-t-4 border-amarelo-ouro border-solid rounded-full animate-spin"></div>
+    </div>
+  )
+}
+
+export default LoadingSpinner
