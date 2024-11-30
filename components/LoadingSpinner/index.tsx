9551da43ab3619fea8d788451e0a95b795be647e
Ailton Duarte <adidas.coyas@gmail.com>
2024-11-30 17:54:11 -0100

    feat: flowbit spinner

--- components/LoadingSpinner/index.tsx
+++ components/LoadingSpinner/index.tsx
@@ -1,10 +1,18 @@
 // LoadingSpinner.js
 import React from "react"
+import { Spinner } from "flowbite-react"
 
 const LoadingSpinner = () => {
   return (
     <div className="flex justify-center items-center h-screen">
-      <div className="w-16 h-16 border-4 border-t-4 border-amarelo-ouro border-solid rounded-full animate-spin"></div>
+      <div className="flex flex-wrap items-center gap-2 h-100 w-auto">
+        <Spinner
+          aria-label="Extra large spinner example"
+          size="xl"
+          color="warning"
+          light={true}
+        />
+      </div>
     </div>
   )
 }
