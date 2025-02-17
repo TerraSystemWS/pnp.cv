b8c820ea6170c47e9dbb7b772b4a91649819d083
Ailton Duarte <adidas.coyas@gmail.com>
2025-02-17 12:06:25 -0100

    fix: add more exceptions

--- components/custom/sidemenu.tsx
+++ components/custom/sidemenu.tsx
@@ -1,23 +1,58 @@
 import React from "react"
 import Link from "next/link"
 import { useRouter } from "next/router"
-import { FaUserAlt, FaStar, FaVoteYea } from "react-icons/fa" // Adicionando ícones para as opções de menu
+import { FaUserAlt, FaStar, FaVoteYea } from "react-icons/fa"
 
-const UserProfileCard = ({ user }: any) => {
+interface UserProfileCardProps {
+  user: string
+}
+
+const UserProfileCard = ({ user }: UserProfileCardProps) => {
   const router = useRouter()
   const pathname = router.pathname
 
-  // Função para verificar se o link está ativo
+  // Helper function to check if the link is active
   const isActive = (path: string) => pathname === path
+
+  // Avatar URL generator
+  const avatarUrl = `https://ui-avatars.com/api/?name=${encodeURIComponent(
+    user
+  )}&background=0D8ABC&color=fff&size=200`
+
+  // Access Links
+  const accessLinks = [
+    {
+      href: "/perfil",
+      icon: <FaUserAlt className="mr-3 text-xl" />,
+      label: "Perfil",
+    },
+    {
+      href: "/perfil/avaliacao",
+      icon: <FaStar className="mr-3 text-xl" />,
+      label: "Avaliar Projetos",
+    },
+    user === "soniarosa" || user === "ailton"
+      ? {
+          href: "/perfil/votacaopublicaStatus",
+          icon: <FaVoteYea className="mr-3 text-xl" />,
+          label: "Resultado da Votação Pública",
+        }
+      : null,
+    user === "soniarosa" || user === "ailton" || user === "Solange Cesarovna"
+      ? {
+          href: "/perfil/avaliacaoStatus",
+          icon: <FaVoteYea className="mr-3 text-xl" />,
+          label: "Resultado da Avaliação do Jurados",
+        }
+      : null,
+  ].filter(Boolean) // Remove any null entries
 
   return (
     <div className="col-span-4 sm:col-span-3">
       <div className="bg-white shadow-xl rounded-lg p-6">
         <div className="flex flex-col items-center">
           <img
-            src={`https://ui-avatars.com/api/?name=${encodeURIComponent(
-              user
-            )}&background=0D8ABC&color=fff&size=200`}
+            src={avatarUrl}
             className="w-32 h-32 bg-gray-300 rounded-full mb-4 border-4 border-yellow-500"
             alt={`Avatar de ${user}`}
           />
@@ -29,68 +64,21 @@ const UserProfileCard = ({ user }: any) => {
             Links de Acesso
           </span>
           <ul className="space-y-6">
-            <li className="flex items-center mb-4">
-              <Link
-                href="/perfil"
-                className={`flex items-center p-3 rounded-lg transition duration-300 ease-in-out ${
-                  isActive("/perfil")
-                    ? "bg-yellow-500 text-white shadow-lg"
-                    : "hover:bg-yellow-100 hover:text-yellow-500"
-                }`}
-              >
-                <FaUserAlt className="mr-3 text-xl" />
-                <span className="text-sm font-semibold">Perfil</span>
-              </Link>
-            </li>
-            <li className="flex items-center mb-4">
-              <Link
-                href="/perfil/avaliacao"
-                className={`flex items-center p-3 rounded-lg transition duration-300 ease-in-out ${
-                  isActive("/perfil/avaliacao")
-                    ? "bg-yellow-500 text-white shadow-lg"
-                    : "hover:bg-yellow-100 hover:text-yellow-500"
-                }`}
-              >
-                <FaStar className="mr-3 text-xl" />
-                <span className="text-sm font-semibold">Avaliar Projetos</span>
-              </Link>
-            </li>
-            {user == "soniarosa" || user == "ailton" ? (
-              <>
-                <li className="flex items-center mb-4">
-                  <Link
-                    href="/perfil/votacaopublicaStatus"
-                    className={`flex items-center p-3 rounded-lg transition duration-300 ease-in-out ${
-                      isActive("/perfil/votacaopublicaStatus")
-                        ? "bg-yellow-500 text-white shadow-lg"
-                        : "hover:bg-yellow-100 hover:text-yellow-500"
-                    }`}
-                  >
-                    <FaVoteYea className="mr-3 text-xl" />
-                    <span className="text-sm font-semibold">
-                      Resultado da Votação Pública
-                    </span>
-                  </Link>
-                </li>
-                <li className="flex items-center mb-4">
-                  <Link
-                    href="/perfil/avaliacaoStatus"
-                    className={`flex items-center p-3 rounded-lg transition duration-300 ease-in-out ${
-                      isActive("/perfil/avaliacaoStatus")
-                        ? "bg-yellow-500 text-white shadow-lg"
-                        : "hover:bg-yellow-100 hover:text-yellow-500"
-                    }`}
-                  >
-                    <FaVoteYea className="mr-3 text-xl" />
-                    <span className="text-sm font-semibold">
-                      Resultado da Avaliação do Jurados
-                    </span>
-                  </Link>
-                </li>
-              </>
-            ) : (
-              <></>
-            )}
+            {accessLinks.map((link, index) => (
+              <li key={index} className="flex items-center mb-4">
+                <Link
+                  href={link.href}
+                  className={`flex items-center p-3 rounded-lg transition duration-300 ease-in-out ${
+                    isActive(link.href)
+                      ? "bg-yellow-500 text-white shadow-lg"
+                      : "hover:bg-yellow-100 hover:text-yellow-500"
+                  }`}
+                >
+                  {link.icon}
+                  <span className="text-sm font-semibold">{link.label}</span>
+                </Link>
+              </li>
+            ))}
           </ul>
         </div>
       </div>
