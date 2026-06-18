7b9ee3b21e6c3cd19d60be2c909b62c6f79db211
Ailton Duarte <Adidas.coyas@gmail.com>
2026-06-18 19:45:44 +0100

    Fix meta tag syntax in _document.tsx

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -3,7 +3,7 @@ import { Html, Head, Main, NextScript } from "next/document";
 export default function Document() {
 	return (
 		<Html lang="pt">
-			<meta name="cf-2fa-verify" content="cL9xR9TP8Q1CN">
+			<meta name="cf-2fa-verify" content="cL9xR9TP8Q1CN" />
 			<Head />
 			<body>
 				<Main />
